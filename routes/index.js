ec7d000d064630b0ede1e161bdc10b49f2d5199e
ZeroPlusKikkok <vilaiphol@hotmail.com>
2022-10-13 20:12:30 +0700

    commit 13/10/2022

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'HomePage AAI' });
+  res.render('index', { title: 'AAI-a5' });
 });
 
 router.get('/signin', (req, res, next) => {
