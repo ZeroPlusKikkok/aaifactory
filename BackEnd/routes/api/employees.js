bf4ed26cdc02499e045b69876e75b4bb5cda1f83
ZeroPlusKikkok <vilaiphol@hotmail.com>
2022-11-08 13:48:52 +0700

    commit 08/11/2022

--- BackEnd/routes/api/employees.js
+++ BackEnd/routes/api/employees.js
@@ -6,8 +6,14 @@ const router = express.Router();
 
 const Employee = require('../../models/employee');
 
-// CRUD ==> Create
+// CRUD ==> Create (POST) — Make something
 router.post('/create', (req, res) => {
+
+  if (!req.body.emCard){
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
+
   const newEmployee = new Employee({
     emCard: req.body.emCard,
     firstName: req.body.firstName,
@@ -16,6 +22,7 @@ router.post('/create', (req, res) => {
     department: req.body.department,
     dateStart: req.body.dateStart,
   });
+
   Employee.create(newEmployee, (err, employee) => {
     if (err) {
       return res.send(JSON.stringify({ error: err }));
@@ -25,7 +32,7 @@ router.post('/create', (req, res) => {
 });
 
 
-// CRUD ==> Read
+// CRUD ==> Read (GET)_- Get something
 router.get('/list', (req, res, next) => {
   Employee.find((err, employees) => {
     if (err) {
@@ -35,9 +42,44 @@ router.get('/list', (req, res, next) => {
   });
 });
 
-// CRUD ==> Update
+// Update (PUT) — Change something
+router.put('/update/:Id', (req, res) => {
 
-// CRUD ==> Delete
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
 
+  const Id = req.params.Id;
+
+  Employee.findByIdAndUpdate(Id, req.body, (err, employees) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.send({
+      message: "Employee was updated successfully!!"
+    });
+    // json(employees);
+  });
+
+});
+
+// Delete (DELETE)– Remove something
+router.delete('/delete/:Id', (req, res) => {
+
+  const Id = req.params.Id;
+
+  Employee.findByIdAndRemove(Id, req.body, (err, employees) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.send({
+      message: "Employee was delete successfully!!"
+    });
+    //json(employees);
+  });
+
+});
 
 module.exports = router;
